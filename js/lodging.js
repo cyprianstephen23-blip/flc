77da02e9a5273b23e052853247fee32b6161c191
cyprianstephen23-blip <cyprianstephen23@gmail.com>
2026-09-10 14:53:01 +0300

    Add files via upload

---
+++ js/lodging.js
@@ -0,0 +1,4 @@
+// Renders Lodging page
+function renderLodging() {
+    document.getElementById('app').innerHTML = `<section class="section"><div class="container"><h2>FRAPG Lodge</h2><p>[Coming soon]</p></div></section>`;
+}
