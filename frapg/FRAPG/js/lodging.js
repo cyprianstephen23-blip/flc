de1290588507605e7984cfaee9c766f52d6d73bf
cyprianstephen23-blip <cyprianstephen23@gmail.com>
2026-09-10 14:52:04 +0300

    Delete frapg directory

--- frapg/FRAPG/js/lodging.js
+++
@@ -1,4 +0,0 @@
-// Renders Lodging page
-function renderLodging() {
-    document.getElementById('app').innerHTML = `<section class="section"><div class="container"><h2>FRAPG Lodge</h2><p>[Coming soon]</p></div></section>`;
-}
